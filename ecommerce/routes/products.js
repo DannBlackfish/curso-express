03d861167ac86280e4bd3ec51eb8efcd878c8c79
DannBlackfish <monsterjunior@outlook.es>
2021-06-25 15:21:12 -0500

    static files

--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const products = [
     {
         name: "Red shoes",
-        price: 75
+        price: 75,
+        image: "https://i.pinimg.com/originals/67/8d/b9/678db979c04155943aabffb424c60c6e.jpg"
     },
     {
         name: "Black bike",
-        price: 300
+        price: 300,
+        image: "https://www.westbrookcycles.co.uk/images/gt-bicycles-avalanche-expert-hardtail-mountain-bike-matte-black-2020-p345710-550215_image.jpg"
     }
 ]
 
