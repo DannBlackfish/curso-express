a89039cbadfbda025d62f3380fe59637f9fe01d1
DannBlackfish <monsterjunior@outlook.es>
2021-06-27 17:19:20 -0500

    conection with mongodb

--- ecommerce/lib/mongo.js
+++ ecommerce/lib/mongo.js
@@ -1,11 +1,12 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const { config } = require("../config");
+const mongoose = require('mongoose')
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = config.dbName;
 
-const MONGO_URI = `mongodb://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?ssl=true&authSource=admin&retryWrites=true` // prettier-ignore
+const MONGO_URI = `mongodb://${USER}:${PASSWORD}@cluster0-shard-00-00.shvvz.mongodb.net:27017,cluster0-shard-00-01.shvvz.mongodb.net:27017,cluster0-shard-00-02.shvvz.mongodb.net:27017/${DB_NAME}?ssl=true&replicaSet=atlas-h6uac6-shard-0&authSource=admin&retryWrites=true&w=majority`  // prettier-ignore
 
 class MongoLib {
   constructor() {
@@ -32,6 +33,39 @@ class MongoLib {
       console.log(error)
     }
   }
+
+  get(collection, id) {
+    return this.connect().then(db => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
+
+  create(collection, data) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then(result => result.insertedId);
+  }
+
+  update(collection, id, data) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then(result => result.upsertedId || id);
+  }
+
+  delete(collection, id) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
+  }
+
 }
 
 module.exports = MongoLib;
