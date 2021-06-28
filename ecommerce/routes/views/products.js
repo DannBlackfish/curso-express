55b5e9cfd1ef8e85780e3029139454e046522ef4
DannBlackfish <monsterjunior@outlook.es>
2021-06-27 23:36:06 -0500

    npm script

--- ecommerce/routes/views/products.js
+++ ecommerce/routes/views/products.js
@@ -1,14 +1,15 @@
+const { config } = require('dotenv');
 const express = require('express');
 const router = express.Router();
 const ProductsService = require('../../services/products');
-
+const { config } = require("../../config")
 const productService = new ProductsService();
 
 router.get('/', async function(req, res, next) {
     const { tags } = req.query
     try {
         const products = await productService.getProducts({ tags });
-        res.render("products", { products });
+        res.render("products", { products, dev: config.dev });
     } catch(err) {
         next(err)
     }
