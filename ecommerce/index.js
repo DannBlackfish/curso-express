a5daa47e541f06309593a60286ac25b980d88517
DannBlackfish <monsterjunior@outlook.es>
2021-06-27 22:10:08 -0500

    passport

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const boom = require("boom");
 const productsRouter = require("./routes/views/products");
 const productsApiRouter = require("./routes/api/products");
+const authApiRouter = require("./routes/api/auth");
 
 const {
   logErrors,
+  wrapErrors,
   clientErrorHandler,
   errorHandler
 } = require("./utils/middlewares/errorsHandlers");
+
+const isRequestAjaxOrApi = require("./utils/isRequestAjaxOrApi");
 
 // app
 const app = express();
@@ -26,14 +31,28 @@ app.set("view engine", "pug");
 // routes
 app.use("/products", productsRouter);
 app.use("/api/products", productsApiRouter);
+app.use("/api/auth", authApiRouter);
 
 // redirect
 app.get("/", function(req, res) {
   res.redirect("/products");
 });
 
+app.use(function(req, res, next) {
+  if (isRequestAjaxOrApi(req)) {
+    const {
+      output: { statusCode, payload }
+    } = boom.notFound();
+
+    res.status(statusCode).json(payload);
+  }
+
+  res.status(404).render("404");
+});
+
 // error handlers
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(clientErrorHandler);
 app.use(errorHandler);
 
